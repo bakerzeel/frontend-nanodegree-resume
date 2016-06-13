b39fa7007de7b4c2a8a66a86c36eb280ba2f7196
bakerzeel <bakerzeel@gmail.com>
2016-06-13 14:35:59 +0100

    1-st checkpoint. Lesson befor encapsulation. No name at header 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,72 @@
+//var formattedName = HTMLheaderName.replace('%data%', 'Genom');
+//var role = HTMLheaderRole.replace('%data%', 'SFBT');
+
+//working with objects
+var skills = ['awesomeness', 'friendiness', 'heart-working'];
+
+var bio = {
+	"name" : "Alex Koz",
+	"role" : "RDA",
+	"contact" : {
+		"phone" : "911",
+		"email" : "bakerzeel@gmail.com",
+		"github" : "bakerzeel",
+		"twitter" : "Alex",
+		"location" : "IE"
+	},
+	"wel_message" : "hey you",
+	"skills" : skills
+}
+
+var work = {
+	"jobs" : [
+	{
+		"employer" : "Zeel",
+		"title" : "Technologist",
+		"location" : "UA",
+		"dates" : "2011-2015",
+		"description" : "baker R&D"
+
+	},
+	{
+		"employer" : "Kerry",
+		"title" : "Technologist",
+		"location" : "IE",
+		"dates" : "2015-curr",
+		"description" : "s_baker"
+	},
+		
+	]
+}
+
+var projects = {
+	"title" : "All about all",
+	"dates" : "2014",
+	"description" : "all around the same",
+	"images" : "blank"
+}
+
+var education = {
+	"schools" : [
+	{
+		"name" : "1",
+		"location" : "Monte",
+		"degree" : "Full middle",
+		"majors" : "12",
+		"dates" : "1994-2005",
+		"url" : "https://www.google.ie/url?sa=t&rct=j&q=&esrc=s&source=web&cd=2&cad=rja&uact=8&ved=0ahUKEwj956PRlZ7NAhXFCMAKHbz1BaIQFgghMAE&url=http%3A%2F%2Fluceschool1.klasna.com%2Fuk%2Fsite%2Fschool-history.html&usg=AFQjCNFGG5IbrypH8KLjhkEgTe0-e85Sxg&sig2=3XAOL9cZ7w50rRTzALc9LQ&bvm=bv.124272578,d.ZGg"
+	}
+	],
+	"onlineCourses" : [
+	{
+		"title" : "Conflictology",
+		"school" : "Bogdanov",
+		"dates" : "2014",
+		"url" : "bogdanov.com.ua"
+	}
+	]
+}
+//$("#main").append(sp_light*meter*nano_s);
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
